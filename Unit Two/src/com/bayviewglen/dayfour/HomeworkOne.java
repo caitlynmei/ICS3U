d2d8cf3a8f24479f60fdb51d72131bf12a8596a8
cmei <cmei@cmeisp3.bayviewglen.ca>
2016-10-24 13:58:52 -0400

    housekeeping

---
+++ Unit Two/src/com/bayviewglen/dayfour/HomeworkOne.java
@@ -0,0 +1,15 @@
+package com.bayviewglen.dayfour;
+
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		
+		final int RANGE_FOR_NUMBERS = 100;
+		
+		int number = (int) ((Math.random() * RANGE_FOR_NUMBERS) + 1);
+		
+		System.out.println("The random number is: " + number);
+		
+	}
+
+}
