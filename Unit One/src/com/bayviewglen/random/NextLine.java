b19f64229378062485f574d52f8964de6f5f66f2
cmei <cmei@cmeisp3.bayviewglen.ca>
2016-11-02 08:29:32 -0400

    Housekeeping

---
+++ Unit One/src/com/bayviewglen/random/NextLine.java
@@ -0,0 +1,35 @@
+package com.bayviewglen.random;
+
+import java.util.Scanner;
+
+public class NextLine {
+	
+	public static void main(String[] args) {
+	
+	/*Scanner keyboard = new Scanner(System.in);
+	
+	System.out.print("Please enter a word: ");
+	String happy = keyboard.nextLine();
+	System.out.println("Ta-daa..." + happy);
+	
+	System.out.print("Please enter another word: ");
+	String happyToo = keyboard.next();
+	System.out.println("Second one ... " + happyToo);
+	*/
+	
+	final int num1 = 4;
+		
+	String s = "crunch";
+	int a = 3, b = 1;
+	
+	System.out.print(s + a + b + a);
+	System.out.println(s + (b + a) + s);
+	
+	System.out.println(a + num1 + "....");
+	
+	
+	//keyboard.close();
+	
+	}
+
+}
