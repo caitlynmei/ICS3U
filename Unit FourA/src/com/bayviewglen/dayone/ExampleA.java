ca4510a81eee60fea741db7ffa64a7f3a4cd775c
cmei <cmei@cmeisp3.bayviewglen.ca>
2016-11-03 13:43:58 -0400

    Unit Four initial Commit

---
+++ Unit FourA/src/com/bayviewglen/dayone/ExampleA.java
@@ -0,0 +1,26 @@
+package com.bayviewglen.dayone;
+
+public class ExampleA {
+
+	public static void main(String[] args) {
+	/*int sum = 0;
+	
+	int current = 1;
+	
+	while (current < 1001){
+		sum += current;
+		current ++;
+		
+	}
+	
+	System.out.println("The sume of the numbers from 1 to 1000 is: " + sum);
+	
+	}*/
+	
+	int x = 11;
+	int y = 5;
+	int mod = x % y;
+	System.out.println("The answer is: " + mod);
+	
+	}
+}
