43596fa7d7b621ceebdbdd8c46137495307d09fc
cmei <cmei@cmeisp3.bayviewglen.ca>
2016-11-09 12:42:04 -0500

    Nov 9

--- Unit FourA/src/com/bayviewglen/assignment/AssignmentTwo.java
+++ Unit FourA/src/com/bayviewglen/assignment/AssignmentTwo.java
@@ -1,11 +1,16 @@
 package com.bayviewglen.assignment;
 
+import java.util.Scanner;
+
 public class AssignmentTwo {
 
 	public static void main(String[] args) {
 	
+		Scanner keyboard = new Scanner (System.in);
+		
 		System.out.println("hi");
 
+		
 	}
 
 }
