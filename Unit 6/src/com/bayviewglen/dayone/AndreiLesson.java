162259f59aafc4588098dff67121b573d8e689ee
cmei <cmei@cmeisp3.bayviewglen.ca>
2016-12-13 15:58:50 -0500

    Unit 6 - Arrays

---
+++ Unit 6/src/com/bayviewglen/dayone/AndreiLesson.java
@@ -0,0 +1,20 @@
+package com.bayviewglen.dayone;
+
+public class AndreiLesson {
+
+	public static void main(String[] args) {
+		
+		String[] arr = {"Example", "okay", "hi"};
+		
+		arr[0] = "kek";
+				
+		int num = arr.length;
+		
+		int num2 = arr[0].length();
+		System.out.println(num2);
+		
+		
+		
+	}
+
+}
