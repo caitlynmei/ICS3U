da0f6ea3e58ba5e87e040610f96a7516996ae76c
cmei <cmei@cmeisp3.bayviewglen.ca>
2017-01-13 15:20:07 -0500

    Housekeeping

--- Unit Nine/src/com/bayviewglen/daytwo/ExampleOneRecursion.java
+++ Unit Nine/src/com/bayviewglen/daytwo/ExampleOneRecursion.java
@@ -4,9 +4,16 @@ public class ExampleOneRecursion {
 
 	public static void main(String[] args) {
 		
+		System.out.println(factorial(5));
 
 	}
 
-	int factorial
+	public static int factorial (int n){
+		if (n == 0 || n == 1){
+			return 1;
+		} else {
+			return n * factorial(n-1);
+		}
+	}
 	
 }
